45ce60f2f7644b624c3926e8867f0c624a64334e
BilgeKaganOzkan <147448844+BilgeKaganOzkan@users.noreply.github.com>
2023-10-10 17:40:59 +0300

    Doxygen format was added for MqttComm.h

--- SerialPortDriver/lib/MqttComm.h
+++ SerialPortDriver/lib/MqttComm.h
@@ -7,6 +7,10 @@
 #include <vector>
 #include "Message.h"
 
+/**
+ * @class MqttComm
+ * @brief MqttComm class connect MQTT Server and publish prepared data to topics as MQTT messages.
+ */
 class MqttComm
 {
 private:
@@ -14,10 +18,39 @@ private:
     mqtt::client* client;
     MqttConfig mqttConfig;
     FailCode<MqttFailCodeType> failCode;
+
+    /**
+     * @brief This function convert IniParserMqttClientConfig Structure type to MqttConfig Structure type.
+     * @param[in] mqttConfigData: IniParser MQTT Client Configuration Structure.
+     * @return N/A
+     */
     void convertIniParserDataToMqttConfig(IniParserMqttClientConfig& mqttConfigData);
 public:
+    /**
+     * @brief This function initialize the isConnect variable and failCode pointer.
+     * @param N/A
+     * @return N/A
+     */
     MqttComm();
+
+    /**
+     * @brief This function delete failCode pointer.
+     * @param N/A
+     * @return N/A
+     */
     ~MqttComm();
+
+    /**
+     * @brief This function connect to the MQTT server.
+     * @param[in] mqttConfigData: IniParser MQTT Client Configuration Structure.
+     * @return N/A
+     */
     void ConnectServer(IniParserMqttClientConfig& mqttConfigData);
+
+    /**
+     * @brief This function publish prepared data to topics as MQTT messages.
+     * @param message: Message object referance.
+     * @return N/A
+     */
     void publishMessage(Message& message);
 };
