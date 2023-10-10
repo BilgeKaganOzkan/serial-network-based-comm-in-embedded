ff817ad109a4959a9bfeb58ffc91236f4b22e799
BilgeKaganOzkan <147448844+BilgeKaganOzkan@users.noreply.github.com>
2023-10-10 17:28:17 +0300

    Doxygen format was added for MessageConfig.h

--- SerialPortDriver/lib/MessageConfig.h
+++ SerialPortDriver/lib/MessageConfig.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <iostream>
 
+/* Message Structure To Be Send To The MQTT Server*/
 struct MessageConfig
 {
-    std::string startCharacters;
-	std::string seperatorCharacters;
-	std::string endCharacters;
-    int seperatedDataCount;
+    std::string startCharacters; // This indicates start characters of the message structure.
+	std::string seperatorCharacters; // This indicates seperator characters of the message structure.
+	std::string endCharacters; // This indicates end characters of the message structure.
+    int seperatedDataCount; // This indicates that how many data will be acquired after parsing the reveived message.
 };
