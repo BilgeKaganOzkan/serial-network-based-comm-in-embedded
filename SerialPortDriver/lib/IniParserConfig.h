2dbb4781436023bf9d3ce3262d3725e982a37ed4
BilgeKaganOzkan <147448844+BilgeKaganOzkan@users.noreply.github.com>
2023-10-10 15:55:52 +0300

    Doxygen format added for IniParserConfig.h

--- SerialPortDriver/lib/IniParserConfig.h
+++ SerialPortDriver/lib/IniParserConfig.h
@@ -2,38 +2,42 @@
 #include <iostream>
 #include "SerialConfig.h"
 
+/*Failcode Types */
 enum IniParserFailCode
 {
-    INIPARSER_OK = 0,
-    INI_FILE_CANNOT_OPEN = 1,
-    PARSING_ERROR = 2
+    INIPARSER_OK = 0, // This indicates that the process was successful.
+    INI_FILE_CANNOT_OPEN = 1, // This indicates that Config.ini file could not opened.
+    PARSING_ERROR = 2 // This indicates that an parsing error occured. For example, config.ini file is corrupt.
 };
 
+/*Serial Port Configuration Structure */
 struct IniParserSerialPortConfig
 {
-	std::string portName;
-	int baudRate;
-	int dataBit;
-	int stopBit;
-	std::string parityBit;
+	std::string portName; // This indicates serial port name.
+	int baudRate; // This indicates baudrate.
+	int dataBit; // This indicates data bit length.
+	int stopBit; // This indicates stop bit length.
+	std::string parityBit; // This indicates parity bit type.
 };
 
+/* MQTT Client Configuration Structure */
 struct IniParserMqttClientConfig
 {
-    std::string serverAdress;
-    int port;
-    std::string topic1;
-    std::string topic2;
-    std::string topic3;
-    std::string topic4;
-    std::string id;
-    int qos;
+    std::string serverAdress; // This indicates MQTT Server ip adress.
+    int port; // This indicates MQTT Server port number.
+    std::string topic1; // This indicates first MQTT topic.
+    std::string topic2; // This indicates second MQTT topic.
+    std::string topic3; // This indicates third MQTT topic.
+    std::string topic4; // This indicates fourth MQTT topic.
+    std::string id; // This indicates unique id for connect MQTT server.
+    int qos; // This indicates QOS number.
 };
 
+/* Message Structure To Be Send To The MQTT Server*/
 struct IniParserMessageConfig
 {
-    std::string startCharacters;
-	std::string seperatorCharacters;
-	std::string endCharacters;
-    int seperatedDataCount;
+    std::string startCharacters; // This indicates start characters of the message structure.
+	std::string seperatorCharacters; // This indicates seperator characters of the message structure.
+	std::string endCharacters; // This indicates end characters of the message structure.
+    int seperatedDataCount; // This indicates that how many data will be acquired after parsing the reveived message.
 };
