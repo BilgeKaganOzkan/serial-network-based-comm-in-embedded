f2916b09755111c9dad44fbea342b4fcd5668891
BilgeKaganOzkan <ozkan.bilge@metu.edu.tr>
2023-10-12 21:00:22 +0300

    mainpages was added for doxygen.

---
+++ GUI/DoxygenFiles/mainpage.h
@@ -0,0 +1,14 @@
+/*
+This file only used for doxygen!!!!
+*/
+
+/**
+ @mainpage Serial Port Driver
+
+ @subsection submodule This project was created by Bilge Kağan ÖZKAN for Kuartis.
+
+If you don't know how to start, you can see <a href="modules.html">Modules Page</a>
+
+<img src="../../DoxygenFiles/logo.png" alt="logo" width="1152" height="507">
+
+*/
